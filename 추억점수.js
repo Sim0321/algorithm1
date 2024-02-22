e98a6bf435410d05e2ae2601446ee0838c7fa05a
Sim <0doogadooga@gmail.com>
2024-02-22 19:58:23 +0900

    추억점수

---
+++ 추억점수.js
@@ -0,0 +1,33 @@
+// 문제 설명
+// 사진들을 보며 추억에 젖어 있던 루는 사진별로 추억 점수를 매길려고 합니다. 사진 속에 나오는 인물의 그리움 점수를 모두 합산한 값이 해당 사진의 추억 점수가 됩니다. 예를 들어 사진 속 인물의 이름이 ["may", "kein", "kain"]이고 각 인물의 그리움 점수가 [5점, 10점, 1점]일 때 해당 사진의 추억 점수는 16(5 + 10 + 1)점이 됩니다. 다른 사진 속 인물의 이름이 ["kali", "mari", "don", "tony"]이고 ["kali", "mari", "don"]의 그리움 점수가 각각 [11점, 1점, 55점]]이고, "tony"는 그리움 점수가 없을 때, 이 사진의 추억 점수는 3명의 그리움 점수를 합한 67(11 + 1 + 55)점입니다.
+
+// 그리워하는 사람의 이름을 담은 문자열 배열 name, 각 사람별 그리움 점수를 담은 정수 배열 yearning, 각 사진에 찍힌 인물의 이름을 담은 이차원 문자열 배열 photo가 매개변수로 주어질 때, 사진들의 추억 점수를 photo에 주어진 순서대로 배열에 담아 return하는 solution 함수를 완성해주세요.
+
+const solution = (name, yearning, photo) => {
+  let obj = {};
+  for (let i = 0; i < name.length; i++) {
+    obj[name[i]] = yearning[i];
+  }
+
+  return photo.map((arr) => arr.reduce((acc, cur) => acc + (obj[cur] || 0), 0));
+};
+
+console.log(
+  solution(
+    ["may", "kein", "kain", "radi"],
+    [5, 10, 1, 3],
+    [
+      ["may", "kein", "kain", "radi"],
+      ["may", "kein", "brin", "deny"],
+      ["kon", "kain", "may", "coni"],
+    ]
+  )
+);
+
+// 해설 및 다른 사람 풀이
+// object를 생성하는 것 까지는 알았지만 forEach를 돌린 후 또 map을 돌렸다. 그냥 reduce 사용하면 해결되는 문제였는데
+
+// 다른 사람 풀이
+return photo.map((v) =>
+  v.reduce((a, c) => (a += yearning[name.indexOf(c)] ?? 0), 0)
+);
