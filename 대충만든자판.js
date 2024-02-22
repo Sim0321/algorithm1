992915ce732c6636351644b00fbec24aa9ab50f7
Sim <0doogadooga@gmail.com>
2024-02-23 02:47:21 +0900

    대충만든자판 reduce 추가

--- 대충만든자판.js
+++ 대충만든자판.js
@@ -40,3 +40,23 @@ console.log(solution(["AA"], ["B"]));
 
 // 해설 및 다른 사람 풀이
 // 최소 횟수를 구해야하므로 Map 자료구조 사용하는 것이 효율적.
+
+// reduce를 활용한 코드
+function solution(keymap, targets) {
+  const answer = [];
+  const map = {};
+  for (const items of keymap) {
+    items
+      .split("")
+      .map(
+        (item, index) =>
+          (map[item] = map[item] < index + 1 ? map[item] : index + 1)
+      );
+  }
+  for (const items of targets) {
+    answer.push(
+      items.split("").reduce((cur, item) => (cur += map[item]), 0) || -1
+    );
+  }
+  return answer;
+}
