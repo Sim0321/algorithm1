1669e213e062e2966c11cc0812405323303d93d3
Sim <0doogadooga@gmail.com>
2024-02-25 14:52:23 +0900

    없는 숫자 더하기

---
+++ 없는숫자더하기.js
@@ -0,0 +1,19 @@
+// 문제 설명
+// 0부터 9까지의 숫자 중 일부가 들어있는 정수 배열 numbers가 매개변수로 주어집니다. numbers에서 찾을 수 없는 0부터 9까지의 숫자를 모두 찾아 더한 수를 return 하도록 solution 함수를 완성해주세요.
+
+const solution = (numbers) => {
+  const arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  let answer = 0;
+  console.log(
+    arr.forEach((num) => {
+      if (!numbers.includes(num)) {
+        answer += num;
+      }
+    })
+  );
+  return answer;
+};
+
+console.log(solution([5, 8, 4, 0, 6, 7, 9]));
+
+// 해설 및 다른 사람 풀이
